06f5f6b61dec51bfb4d0efaf811d8802f5e43bf9
Rachit Shukla <rachit.shukla28@gmail.com>
2024-01-18 16:45:45 +0530

    Add comment in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -8,3 +8,6 @@ ReactDOM.createRoot(entryPoint).render(<App />);
 
 //use Vite for fast server start and HMR (Hot Module Replacement).
 //npm create vite@latest my-react-app --template react
+
+//you can only learn Git by doing it. The more you do it, the better you get at it.
+//ChatGPT has made life much easier for a developer, provided he knows how to use it.
